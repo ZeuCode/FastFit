1e653c9db0eb811afec8cbbf3cc023a7fd771773
unknown <ricardorodriguez.cc14@gmail.com>
2023-04-12 16:20:50 -0500

    Se creo el archivo data.js

---
+++ data.js
@@ -0,0 +1,55 @@
+module.exports = function () {
+  var data = {
+    clients: [
+      {
+        id: 1,
+        userName: "Ricardo14",
+        password: "holaMundo",
+        names: "Cesar Ricardo",
+        lastNames: "Rodriguez Ccorahua",
+        emailAddress: "ricardito12hola@gmail.com",
+        phoneNumber: "976238462",
+        age: 23,
+        UserStatus_id: 1,
+        Gender_id: 1,
+      },
+      {
+        id: 2,
+        userName: "Miguelaso",
+        password: "adiosMiguel",
+        names: "Miguel Angel",
+        lastNames: "Huaman Sosa",
+        emailAddress: "miguelito15as@gmail.com",
+        phoneNumber: "956336482",
+        age: 25,
+        UserStatus_id: 1,
+        Gender_id: 1,
+      },
+      {
+        id: 3,
+        userName: "Marcos15",
+        password: "messi23fw",
+        names: "Marcos Alonso",
+        lastNames: "Salinas Cotrina",
+        emailAddress: "mymessi10best@gmail.com",
+        phoneNumber: "994233461",
+        age: 33,
+        UserStatus_id: 1,
+        Gender_id: 1,
+      },
+      {
+        id: 4,
+        userName: "princesa12",
+        password: "rockstarforever",
+        names: "Angela Jimena",
+        lastNames: "Valenzuela Hoyos",
+        emailAddress: "siemprehappy15@gmail.com",
+        phoneNumber: "956438361",
+        age: 20,
+        UserStatus_id: 1,
+        Gender_id: 2,
+      },
+    ],
+  };
+  return data;
+};
