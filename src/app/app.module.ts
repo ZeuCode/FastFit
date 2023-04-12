522bf16ac58afc2aa36f24b0888adac6eb9adb7e
juan c <juan.carlotg12@gmail.com>
2023-04-12 17:52:50 -0500

    agrego review y review-list en comnponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ReviewComponent } from './componet/review/review.component';
+import { ReviewListComponent } from './componet/review/review-list/review-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ReviewComponent,
+    ReviewListComponent
   ],
   imports: [
     BrowserModule,
