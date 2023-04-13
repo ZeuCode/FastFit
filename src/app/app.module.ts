6a1ddea53080f5b7c881fc26f1ae223780069588
juan c <juan.carlotg12@gmail.com>
2023-04-12 21:15:08 -0500

    add git review-component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,39 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ReviewComponent } from './componet/review/review.component';
-import { ReviewListComponent } from './componet/review/review-list/review-list.component';
+import { ReviewComponent } from './component/review/review.component';
+import { ReviewListComponent } from './component/review/review-list/review-list.component';
 import { HttpClientModule } from '@angular/common/http';
 import { MatTableModule } from '@angular/material/table';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select'
+import { MatDatepickerModule } from '@angular/material/datepicker'
+import { MatNativeDateModule } from '@angular/material/core'
+import { ReviewInsertComponent } from './component/review/review-insert/review-insert.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     ReviewComponent,
-    ReviewListComponent
+    ReviewListComponent,
+    ReviewInsertComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     MatTableModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatInputModule,
+    MatSelectModule,
+    MatDatepickerModule,
+    MatNativeDateModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/component/review/review-insert/review-insert.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-review-insert',
+  templateUrl: './review-insert.component.html',
+  styleUrls: ['./review-insert.component.css']
+})
+export class ReviewInsertComponent {
+
+}
