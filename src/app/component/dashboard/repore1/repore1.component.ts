fc8f74ff81a708d61257485d1249cdb2d211ee18
CortexSky <christianprada2@gmail.com>
2023-06-21 22:42:41 -0500

    reporte1

---
+++ src/app/component/dashboard/repore1/repore1.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-repore1',
+  templateUrl: './repore1.component.html',
+  styleUrls: ['./repore1.component.css']
+})
+export class Repore1Component {
+
+}
